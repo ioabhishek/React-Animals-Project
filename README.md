e59f8c71fc9a112191f6cb681ec4bf3cb1b26e6a
ioabhishek <65117467+ioabhishek@users.noreply.github.com>
2023-03-05 00:52:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Animals-Project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/animals-ioabhishek)
---
+++ src/AnimalShow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AnimalShow({ type }) {
+  return (
+    <>
+      <div>{type}</div>
+    </>
+  );
+}
+
+export default AnimalShow;
